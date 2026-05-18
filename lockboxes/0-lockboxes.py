be33adc35268ae1946c82954e42eea0e83a129ee
steven <12992@holbertonstudents.com>
2026-05-18 13:20:08 +0200

    lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that checks if all boxes can be unlocked."""
+
+
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be opened.
+
+    Args:
+        boxes (list): List of lists containing keys.
+
+    Returns:
+        bool: True if all boxes can be opened, otherwise False.
+    """
+    opened = set([0])
+    keys = [0]
+
+    while keys:
+        current = keys.pop()
+
+        for key in boxes[current]:
+            if key < len(boxes) and key not in opened:
+                opened.add(key)
+                keys.append(key)
+
+    return len(opened) == len(boxes)
+    
